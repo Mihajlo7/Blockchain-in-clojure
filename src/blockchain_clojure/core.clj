f3b1aa2ab4ef57804aa6f43273bb844c2fecdb2b
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-12-10 16:26:09 +0100

    Created methods for creating new block and adding in chain and getting last block from chain

--- src/blockchain_clojure/core.clj
+++ src/blockchain_clojure/core.clj
@@ -1,1 +1,25 @@
-(ns blockchain-clojure.core)
+(ns blockchain-clojure.core
+  (:import (java.time LocalDateTime)))
+;)
+; Defined chain  )
+(def chain (atom []))
+
+(def new-transactions (atom []))
+
+(defn create-new-block [nonce, previous-hash, hash]
+  "Creating new block and adding in chain"
+  (let [new-block {:index        (inc (count @chain))
+                   :timestamp    (LocalDateTime/now)
+                   :nonce        nonce
+                   :previousHash previous-hash
+                   :hash         hash
+                   :transactions @new-transactions
+                   }]
+    (swap! chain conj new-block)
+    (reset! new-transactions [])
+    new-block)
+  )
+
+(defn get-last-block []
+  "Getting the last block from chain"
+  (last @chain))
