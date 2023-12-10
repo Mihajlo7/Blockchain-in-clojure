75482332f53b857f9da2b5994e5a91170aede30e
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-12-10 18:03:14 +0100

    Implemented new functions and tested in repl

--- src/blockchain_clojure/core.clj
+++ src/blockchain_clojure/core.clj
@@ -4,7 +4,7 @@
 ; Defined chain  )
 (def chain (atom []))
 
-(def new-transactions (atom []))
+(def pending-transactions (atom []))
 
 (defn create-new-block [nonce, previous-hash, hash]
   "Creating new block and adding in chain"
@@ -13,13 +13,35 @@
                    :nonce        nonce
                    :previousHash previous-hash
                    :hash         hash
-                   :transactions @new-transactions
+                   :transactions @pending-transactions
                    }]
     (swap! chain conj new-block)
-    (reset! new-transactions [])
+    (reset! pending-transactions [])
     new-block)
   )
 
 (defn get-last-block []
   "Getting the last block from chain"
   (last @chain))
+
+(defn get-index-for-transaction []
+
+  (if (empty? @chain)
+    1
+    (-> (get-last-block)
+        (get :index)
+        inc)))
+
+(defn create-new-transaction [amount sender receiver]
+  "Creating new transaction"
+  (let [new-transaction {:amount amount
+                         :sender sender
+                         :receiver receiver}]
+    (swap! pending-transactions conj new-transaction))
+(get-index-for-transaction))
+
+;(defn sha-256 [input])
+
+(defn hash-block [block]
+  "Function return hash data for block"
+  (let [block-data (str (get-in block [:index :timestamp :nonce :previousHash]))]))
